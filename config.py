1577879cdc638ab318f48319d6d87d8a14cf70e4
Zyx <esingzhang@stu.xjtu.edu.cn>
2023-01-18 11:22:39 +0800

    :construction: regular update

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ if 'cool_group' not in config:
     logger.warning('未发现配置项 `cool_group` , 采用默认值: []')
 
 if 'cool_time' not in config:
-    logger.warning('未发现配置项 `cool_time` , 采用默认值: 100')
+    logger.warning('未发现配置项 `cool_time` , 采用默认值: 200')
 
 cool_group = config.get('cool_group', [])
-cool_time = config.get('cool_time', 1)
+cool_time = config.get('cool_time', 200)
 
 if cool_time == 'random':
-    cool_time = random.randint(100, 150)
+    cool_time = random.randint(150, 200)
 
 cool_message = "不觉得很酷吗？\n" \
                "作为一个QQ Bot，我觉得这实在是太酷了，\n" \
